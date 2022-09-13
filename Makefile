7d4ef1c9a0e67aa13598d0d83d3adb76c62f6fc5
seunghyun <kmasolo@naver.com>
2022-09-14 05:59:26 +0900

    edit

---
+++ Makefile
@@ -0,0 +1,11 @@
+all : add_nbo
+
+add_nbo: add_nbo.o
+	g++ -o add_nbo add_nbo.o
+
+add_nbo.o: add_nbo.cpp
+
+clean:
+	rm -f add_nbo *.o
+
+
--- add_nbo.cpp
+++ add_nbo.cpp
@@ -1,23 +1,31 @@
+
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <netinet/in.h>
 
-int main(){
+int main(int argc, char* argv[]){
+
+	if(argc != 3) return 0;
+
 	FILE *fp1, *fp2;
 
-	fp1 = fopen("thousand.bin", rb);
+	fp1 = fopen(argv[1], "rb");
 
 	uint32_t buffer1;
-	uint32_t a1 = fread(&buffer1, 1, 1, fp1);
+	uint32_t a1 = fread(&buffer1, 1, sizeof(buffer1), fp1);
 
-	fp2 = fopen("five-hundred.bin", rb);
+	fp2 = fopen(argv[2], "rb");
 
 	uint32_t buffer2;
-	uint32_t a2 = fread(&buffer2, 1, 1, fp2);
+	uint32_t a2 = fread(&buffer2, 1, sizeof(buffer2), fp2);
 
-	a1 = uint32_t ntohl(a1);
-	a2 = uint32_t ntohl(a2);
+	a1 = ntohl(a1);
+	a2 = ntohl(a2);
+	
+	fclose(fp1);
+	fclose(fp2);
 
-	printf("%d\n", a1+a2);
+	printf("%d(0x%x) + %d(0x%x) = %d(0x%x) \n", a1, a1, a2, a2, a1+a2);
 }
 
